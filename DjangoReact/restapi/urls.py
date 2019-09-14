fe17b147c8e7b0a3bfd3ae91f99ac503ba93b92a
unknown <fjsszx@gmail.com>
2019-09-14 12:27:19 -0500

    Commit antes del primer test del funcionamiento de la api

--- DjangoReact/restapi/urls.py
+++ DjangoReact/restapi/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from rest_framework import routers
+from .api import LeadViewSet
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('restapi.urls'))
-]
+router = routers.DefaultRouter()
+router.register('api/Leads', LeadViewSet, 'restapi')
+
+urlpatterns = router.urls
